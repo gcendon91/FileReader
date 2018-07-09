a4195d83ae34981b42d1b7f3e22121d74f518769
Gonzalo <Gonzalo@DESKTOP-8MB0VB1>
2018-07-08 22:45:44 -0300

    Add project files.

---
+++ GonzaloCendon/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GonzaloCendon
+{
+    public class Profile
+    {        
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string CurrentRole { get; set; }
+        public string Country { get; set; }
+        public string Industry { get; set; }
+        public int NumberOfRecommendations { get; set; }
+        public int NumberOfConnections { get; set; }
+
+        public Profile() { }                 
+               
+    }
+}
---
+++ GonzaloCendon/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GonzaloCendon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                        
+            List<Profile> profiles = FileReader(@"C:\Users\gcendon.XPLAN\Downloads\people.in");
+
+            profiles = profiles.OrderByDescending(Profile => Profile.NumberOfConnections).ToList();
+
+            //profiles.OrderByDescending(x => x.NumberOfConnections).ThenBy(x => x.NumberOfRecommendations);
+
+
+            foreach (var s in profiles)
+            {
+                Console.WriteLine(s.PersonId);
+            }
+
+            Console.ReadKey();
+                               
+        }
+        public static List<Profile> FileReader(string path)
+        {            
+            string line;
+            StreamReader file = new StreamReader(path);
+            List<Profile> profiles = new List<Profile>();
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] splits = line.Split('|');
+                if (splits.Length!=8)
+                {
+                    throw new Exception ("Invalid number of fields");
+                }              
+                
+                Profile profile = new Profile();
+                profile.PersonId = (Convert.ToInt32(splits[0]));
+                profile.Name = splits[1];
+                profile.LastName = splits[2];
+                profile.CurrentRole = splits[3];
+                profile.Country = splits[4];
+                profile.Industry = splits[5];
+                profile.NumberOfRecommendations = (Convert.ToInt32(splits[6]));
+                profile.NumberOfConnections = (Convert.ToInt32(splits[7]));
+                profiles.Add(profile);
+            }
+            return profiles;
+        }
+    }
+}
+
